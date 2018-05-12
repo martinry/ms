aa270b09cc1dd666b2d1949a3ce7d8e9c2c93e96
Martin Rydén <pemryd@gmail.com>
2018-05-12 16:54:49 +0200

    Update reformat_wide.py

--- reformat_wide.py
+++ reformat_wide.py
@@ -11,6 +11,7 @@ from collections import defaultdict
 
 start = time.clock()
 
+
 include_num_replicates = 7
 
 class Protein(object):
@@ -24,21 +25,26 @@ class Protein(object):
 pdict = defaultdict(list)
 
 conds = []
-#reps = set()
 
 # def parse():
-with open('/Users/martin/Desktop/Proteomics_VT18/0__PrepareData/0_Source/Elin/elin.body.txt', 'r') as report:
+with open('EF.MM.Body_donors.OA.180404.txt', 'r') as report:
     next(report)
     for line in report:
         line = line.strip()
         line = line.split('\t')
         condition = line[0]
-        if condition not in conds: conds.append(condition)
+        if condition not in conds:
+            conds.append(condition)
         replicate = int(line[2])
         #if replicate not in reps: reps.add(replicate)
         
         accession = line[3].split(';')[0]
-        quantity = line[4].replace(",", ".")
+        
+        quantity = line[4]
+        quantity = quantity.replace('"','')
+        quantity = quantity.replace(",", ".")
+        
+        
 
         if replicate <= include_num_replicates:
             prot = Protein(accession, condition, replicate, quantity)
@@ -47,7 +53,9 @@ with open('/Users/martin/Desktop/Proteomics_VT18/0__PrepareData/0_Source/Elin/el
 
 # For every accession number as key, initiate list of length (num conditions * num replicates)
 pmatrix = {p:[0]*len(conds)*include_num_replicates for p in pdict.keys()}
-    
+
+
+
 # def populate_matrix():
 for acc, plist in pdict.items():
     for protein in plist:
@@ -58,30 +66,18 @@ for acc, plist in pdict.items():
 
 
 
-with open('elin.b123.txt', 'w') as fw:
-    header = '\t'.join(["%s.%s"%(x,y) for x in conds for y in range(1,include_num_replicates+1)])
+with open('OA.donors.body.full.wide.csv', 'w') as fw:
+    header = ','.join(["%s.%s"%(x,y) for x in conds for y in range(1,include_num_replicates+1)])
     fw.write('Accession')
-    fw.write('\t')
+    fw.write(',')
     fw.write(header)
     fw.write('\n')
     for key, value in pmatrix.items():
-        value = '\t'.join([str(x) for x in value])
+        value = ','.join([str(x) for x in value])
         fw.write(key)
-        fw.write('\t')
+        fw.write(',')
         fw.write(value)
         fw.write('\n')
 
 
 print(time.clock() - start)
-
-"""
-for acc, plist in pdict.items():
-    print(acc)
-    mlist = '\t'.join([protein.condition for protein in plist])
-    print(mlist)
-    rlist = '\t'.join([str(protein.replicate) for protein in plist])
-    print(rlist)
-    qlist = '\t'.join([protein.quantity for protein in plist])
-    qlist = '%s\t%s' % (acc, qlist)
-   # print(qlist)
-"""
